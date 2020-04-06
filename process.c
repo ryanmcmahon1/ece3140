b94bc93488913f27a67c26e253b172bd37eb82bf
ryanmcmahon1 <51797981+ryanmcmahon1@users.noreply.github.com>
2020-04-06 16:53:35 -0500

    Update process.c

--- process.c
+++ process.c
@@ -2,92 +2,135 @@
 #include <stdlib.h>
 #include <MK64F12.h>
 
-process_t* process_queue = NULL;
-process_t* current_process = NULL;
+// globals
+process_t* process_queue= NULL;
+process_t* current_process= NULL;
 
-// process_state struct, with typedef process_t
 struct process_state {
-	int n;                        // Size of stack frame
-	unsigned int* original_sp;    // Original stack pointer
-	unsigned int* sp;             // Current Stack pointer
-	struct process_state* next_p; // Next process
+	// vars needed for process state
+	unsigned int* sp; // stack pointer
+	unsigned int* osp; // original stack pointer
+	unsigned int size; // size of process
+	unsigned int process_status; // status
+	struct process_state* next; // next process ( for queue)
 };
 
+// This function sets up the PIT timer and enables interrupts
+void pit_setup() {
+	SIM->SCGC6 = SIM_SCGC6_PIT_MASK; // Enable clock to PIT module
+	PIT->MCR =0x00; // Allows me to turn timer on
+	PIT->CHANNEL[0].LDVAL = SystemCoreClock /10000; // ~1 second
+	PIT->CHANNEL[0].TCTRL |= (1 << 1); // Enable Interrupts
+}
+
 // helper method that appends a process to the end of a queue
-void enqueue(*process_t proc) {
-	
-	// Case 1: queue is empty, so add this process to the head
+void enqueue(process_t* proc) {
+	// if queue empty, given process is the head
 	if (process_queue == NULL) {
 		process_queue = proc;
-		proc->next_p = NULL;
+		proc->next = NULL;
 	}
-	
-	// Case 2: queue is nonempty, so add this process to the end
-	else {
-		temp_p = process_queue;
-		
-		while (temp_p->next_p != NULL) {
-			// Continue iterating until we get to the last element
-			temp_p = temp_p->next_p;
+
+	// otherwise, add given process to the end of the queue
+	else { 
+		process_t* temp = process_queue;
+
+		// find end of queue
+		while ( temp->next != NULL ) {
+			temp = temp->next;
 		}
-		
-		// Set next pointer of this final element
-		temp_p->next_p = proc;
-		proc->next_p = NULL;
+		// put process at the end
+		temp->next = proc;
+		proc->next = NULL;
 	}
 }
 
-// helper method that removes and returns process at head of queue
-process_t* dequeue() {
-	
-	// Case 1: if queue is empty, we have nothing to return
+process_t* dequeue(void) {
+	// if queue is empty, return NULL
 	if (process_queue == NULL) {
 		return NULL;
 	}
-	
-	// Case 2: queue is nonempty, return first element and adjust pointers
+
+	// otherwise, return head process and set the next process as the head
 	else {
 		// temporary pointer to keep track of first element
-		temp_p = process_queue;
-		process_queue = temp_p->next_p;
-		return temp_p;
+		process_t* temp = process_queue;
+		process_queue = temp->next;
+		return temp;
 	}
 }
-
-// Creates a process that starts at function f with stack size n
+			
+// Creates a process that executes function f with stack size n	
 int process_create (void (*f)(void), int n) {
-	
-	// Creating new process_t struct
-	process_t* proc;
-	
-	// Allocating memory for proc
-	proc = malloc(sizeof(process_t));
+	// allocate memory for new process
+	process_t* p = (process_t*) malloc( sizeof(process_t));
 	// Returning -1 if malloc fails
 	if (proc == NULL) return -1;
-	
-	// Allocating memory for the process state on the stack
+
+	// allocate memory for new process state on the stack
 	unsigned int* sp = process_stack_init(f, n);
 	// Returning -1 if stack can't be allocated
 	if (sp == NULL) return -1;
+
+	// Assigning data to the struct
+	p->sp = sp;
+	p->osp = process_queue->sp;
+	p->size = n;
+	p->process_status = 0;
+	p->next = NULL;
+
+	// adds process to the queue
+	enqueue( p );
 	
-	// Assigning fields to the struct
-	proc->n = n;
-	proc->original_sp = sp;
-	proc->sp = sp;
-	proc->next_p = NULL;
-	
-	// Enqueueing this process to the process queue
-	enqueue(proc);
-	
-	// If everything else succeeded, return 0
+	// if no error, return 0
 	return 0;
 }
 
-// Sets up processes and calls process begin
 void process_start (void) {
-	
+
+	//enable inturupts
 	NVIC_EnableIRQ(PIT0_IRQn);
 	
-	// Initiate first process
-	process_begin();	
+	//set up PIT
+	pit_setup();
+
+	if (process_queue != NULL) {
+		current_process = dequeue();
+	}
+
+	// begin
+	process_begin();
 }
+
+unsigned int* process_select(unsigned int* cursp) {
+	// no currently running process or process terminated
+	if ( current_process == NULL ) {
+		current_process = process_queue;
+	}
+	else if (cursp == NULL) {
+		process_stack_free( current_process->osp, current_process->size);
+		current_process = dequeue();
+	}
+	else { // stopped a process
+		//add back to queue
+		enqueue(current_process);
+		
+		// make temp
+		process_t* temp = current_process;
+	
+		// get next process
+		current_process = dequeue();
+	
+		// fix the stack pointer and next
+		temp->sp = cursp;
+		temp->next = NULL;
+	}
+	
+		// if no next process. return NULL
+		if ( current_process == NULL ) { return NULL; }
+		
+		// otherwise return next stack pointer
+		return current_process->sp;
+}
+
+
